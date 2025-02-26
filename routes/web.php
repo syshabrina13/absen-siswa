48fc3928642b0af445444b782334368576938734
syshabrina13 <syarifahshabrina8@gmail.com>
2025-02-26 14:04:52 +0700

    add feat dashboard and login

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Matrix Admin', function()
+{
+return view('index',[
+    "menu"=>"dashboard"
+]);
+})->name('Matrix Admin');
+
+Route::get('/Login', function()
+{
+return view('login',[
+    "menu"=>"login"
+]);
+})->name('Login');
